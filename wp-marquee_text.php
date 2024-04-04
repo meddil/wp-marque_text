86ef2bdf7eadbe9c788501b3a90c798e378f6cce
meddil <med.maaoui.007@gmail.com>
2024-04-04 19:38:38 +0000

    Add submenu for plugin in dashboard under appearance

--- wp-marquee_text.php
+++ wp-marquee_text.php
@@ -2,14 +2,37 @@
 /**
 * Plugin Name: Marquee Running Text
 * Plugin URI: https://www.github.com/meddil/wp-marquee_text
-* Description: ...
+* Description: Add marquee running text to page
 * Version: 0.1
 * Author: Med Maaoui
 * Author URI: https://www.github.com/meddil
 **/
 
-function marquee_content($atts) {
+// initial code
+function marquee_content() {
     $content = '<marquee>Running text here</marquee>';
     return $content;
 }
 add_shortcode('marquee', 'marquee_content');
+
+
+
+
+
+// add shortcut in dashboard under "appearence"
+function marquee_plugin_menu() {
+    add_submenu_page(
+        'themes.php', 
+        'Add A Marquee Running Text', 
+        'Marquee Running Text', 
+        'manage_options',
+        'marquee-plugin',
+        'marquee_plugin_page_display'
+    );
+}
+
+add_action('admin_menu', 'marquee_plugin_menu');
+
+function marquee_plugin_page_display() {
+    echo '<div class="wrap"><h1>Add Marquee Running Text</h1></div>';
+}
