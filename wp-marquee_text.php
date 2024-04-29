32c46cfd287fe4701e59668faeb2ccfb495a4503
meddil <med.maaoui.007@gmail.com>
2024-04-29 18:23:40 +0100

    Simple marquee

--- wp-marquee_text.php
+++ wp-marquee_text.php
@@ -3,7 +3,7 @@
 * Plugin Name: Marquee Running Text
 * Plugin URI: https://www.github.com/meddil/wp-marquee_text
 * Description: Add marquee running text to page
-* Version: 0.1
+* Version: 0.0.1
 * Author: Med Maaoui
 * Author URI: https://www.github.com/meddil
 **/
@@ -20,19 +20,52 @@ add_shortcode('marquee', 'marquee_content');
 
 
 // add shortcut in dashboard under "appearence"
-function marquee_plugin_menu() {
-    add_submenu_page(
-        'themes.php', 
-        'Add A Marquee Running Text', 
-        'Marquee Running Text', 
-        'manage_options',
-        'marquee-plugin',
-        'marquee_plugin_page_display'
-    );
+add_action('admin_menu', 'marquee_running_text_menu');
+function marquee_running_text_menu() {
+    add_menu_page('Marquee Running Text', 'Marquee Running Text', 'manage_options', 'marquee-running-text', 'marquee_running_text_page');
+}
+//interface
+function marquee_running_text_page() {
+    $marquee_text = isset($_POST['marquee_text']) ? sanitize_text_field($_POST['marquee_text']) : '';
+    $shortcode = '[running_text]';
+
+    if ($marquee_text) {
+        update_option('marquee_text', $marquee_text);
+    }
+
+    ?>
+    <div class="wrap">
+        <h2>Marquee Running Text</h2>
+        <form method="post" action="">
+            <label for="marquee-text">Marquee Text:</label>
+            <textarea id="marquee-text" name="marquee_text" rows="4" cols="50"><?php echo esc_textarea($marquee_text); ?></textarea><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+        <p><?php echo esc_html("Shortcode: ") . $shortcode; ?> <button onclick="copyShortcode()">Copy</button></p>
+        <p><?php echo esc_html("Submit the text and copy/paste the shortcut code to your page builder"); ?></p>
+    </div>
+
+    <script>
+        function copyShortcode() {
+            var shortcode = document.createElement("textarea");
+            shortcode.value = "<?php echo esc_js($shortcode); ?>";
+            document.body.appendChild(shortcode);
+            shortcode.select();
+            document.execCommand("copy");
+            document.body.removeChild(shortcode);
+            alert("Shortcode copied!");
+        }
+    </script>
+    <?php
 }
 
-add_action('admin_menu', 'marquee_plugin_menu');
-
-function marquee_plugin_page_display() {
-    echo '<div class="wrap"><h1>Add Marquee Running Text</h1></div>';
+//shortcode generate
+function marquee_text_shortcode($atts, $content = null) {
+    $marquee_text = get_option('marquee_text');
+    if ($marquee_text) {
+        return '<marquee behavior="scroll" direction="left">' . esc_html($marquee_text) . '</marquee>';
+    } else {
+        return '';
+    }
 }
+add_shortcode('running_text', 'marquee_text_shortcode');
